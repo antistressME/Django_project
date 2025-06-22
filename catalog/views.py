95dc4177150bf10272bd0a51265adc68aaf4e636
Anastasia <antipinaanastasia0112@gmail.com>
2025-06-22 15:08:30 +0300

    update path in templates

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, "catalog/home.html")
+    return render(request, "home.html")
 
 def contacts(request):
-    return render(request, "catalog/contacts.html")
+    return render(request, "contacts.html")
