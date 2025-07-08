2a545f2f95cbbcb278cff67261594c71dcddb1a5
Anastasia <antipinaanastasia0112@gmail.com>
2025-07-08 20:25:09 +0300

    update templates, create base template

--- catalog/management/commands/add_product.py
+++ catalog/management/commands/add_product.py
@@ -7,28 +7,21 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
 
-        # category1, _ = Category.objects.get_or_create(name='vegetables')
-        # category2, _ = Category.objects.get_or_create(name='fruits')
-        # category3, _ = Category.objects.get_or_create(name='berries')
-        #
-        # products = [
-        #     {'name': 'raspberry', 'description': 'test', 'category': category3, 'price': '350'},
-        #     {'name': 'carrot', 'description': 'test', 'category': category1, 'price': '55'},
-        #     {'name': 'pear', 'description': 'test', 'category': category2, 'price': '239'},
-        #     {'name': 'strawberry', 'description': 'test', 'category': category3, 'price': '299'},
-        # ]
-        #
-        # for product_data in products:
-        #     product, created = Product.objects.get_or_create(**product_data)
-        #     if created:
-        #         self.stdout.write(self.style.SUCCESS(f'Successfully added product: {product.name}'))
-        #     else:
-        #         self.stdout.write(self.style.WARNING(f'Product already exists: {product.name}'))
+        category1, _ = Category.objects.get_or_create(name='vegetables')
+        category2, _ = Category.objects.get_or_create(name='fruits')
+        category3, _ = Category.objects.get_or_create(name='berries')
 
+        products = [
+            {'name': 'raspberry', 'description': 'test', 'category': category3, 'price': '350'},
+            {'name': 'carrot', 'description': 'test', 'category': category1, 'price': '55'},
+            {'name': 'pear', 'description': 'test', 'category': category2, 'price': '239'},
+            {'name': 'strawberry', 'description': 'test', 'category': category3, 'price': '299'},
+        ]
 
-        call_command('loaddata', 'category_fixture.json')
-        self.stdout.write(self.style.SUCCESS('Successfully loaded data from category_fixture'))
-
-        call_command('loaddata', 'category_fixture.json')
-        self.stdout.write(self.style.SUCCESS('Successfully loaded data from product_fixture'))
+        for product_data in products:
+            product, created = Product.objects.get_or_create(**product_data)
+            if created:
+                self.stdout.write(self.style.SUCCESS(f'Successfully added product: {product.name}'))
+            else:
+                self.stdout.write(self.style.WARNING(f'Product already exists: {product.name}'))
 
--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
+from .models import Product
 
 def home(request):
-    return render(request, "home.html")
+    products = Product.objects.all()
+    context = {
+        "products": products,
+    }
+    return render(request, "home.html", context=context)
 
 def contacts(request):
     return render(request, "contacts.html")
